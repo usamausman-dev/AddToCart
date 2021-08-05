f6bdcb32e98a39eb3f5bd0e25d40b3f416dd192d
Usama Usman <uusman004@gmail.com>
2021-08-05 21:23:37 +0500

    Data Duplication of Objects removed with increment in thier Quantity

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,5 +1,3 @@
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
     apiKey: "AIzaSyB9ZJX5WgBJJl4ENThZB9yIRoJFqd9lfJk",
     authDomain: "addtocart-5ce35.firebaseapp.com",
